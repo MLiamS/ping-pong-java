9fce28f705b70e8abb15e08d56428c79f8f45c7e
Evan Bishop and Liam Stabeno <evan@>
2017-03-15 10:53:44 -0700

    complete the ping-pong app

--- src/main/java/PingPong.java
+++ src/main/java/PingPong.java
@@ -1,4 +1,3 @@
-
 import java.util.ArrayList;
 import java.util.List;
 
@@ -6,6 +5,17 @@ public class PingPong {
 
   public List<Object> runPingPong(int countUpTo){
     List<Object> result = new ArrayList<Object>();
+    for (int i = 1; i <= countUpTo; i++){
+      if ((i % 3 == 0) && (i % 5 == 0)){
+      result.add("ping-pong");
+      } else  if (i % 3 == 0){
+        result.add("ping");
+      } else if (i % 5 == 0){
+        result.add("pong");
+      }  else {
+      result.add(i);
+      }
+    }
     return result;
   }
 
